ecda38058b0e768b31278cd0a0e51d2ff0e0acf7
Jason Liang <jjjasonliang@gmail.com>
2018-07-05 17:54:12 +0900

    strings

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -2,7 +2,31 @@
 
 import UIKit
 
-var str1: String = "Hello, playground"
-var str: Int = 30
+var str: String = "Hello, playground"
+var FirstName = "Jack"
+var LastName = "Chen"
+
+var fullName = FirstName + " " + LastName
+var age = 300
+var fullName2 = "\(FirstName)\(LastName) is \(age)"
+
+fullName.append(" XDD")
+
+var bookTitle = "hello my name is xddddd"
+bookTitle = bookTitle.capitalized
+
+var allCaps = "XDDDDDDD HOWS IT GOING BROOOOO"
+
+var lowercase = allCaps.lowercased().capitalized
+
+// censor: XD, XDD, how's life
+
+var samplesentence = "XDD, how's life my dude. How you doing today XD."
+
+if samplesentence.contains("XDD") || samplesentence.contains("how's life")||samplesentence.contains("XD") {
+    samplesentence.replacingOccurrences(of: "XDD", with: "Takoyaki")
+    samplesentence.replacingOccurrences(of: "XD", with: "Soba")
+    samplesentence.replacingOccurrences(of: "how's life", with: "it's real good")
+}
 
 
