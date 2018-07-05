7cd07392da2be7f093435a5d0d0831a30c4c6271
Jason Liang <jjjasonliang@gmail.com>
2018-07-06 08:43:26 +0900

    numbers

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Type inference
+var age = 40
+
+// Explicitly declared type
+var weight: Int = 200 // Int
+
+var doubleNum: Double = 1202431312131312424231
+
+// In swift double is more preferable than float
+var defaultisdouble = 3.4242
+
+var pi: Float = 3.14
+
+// have error when you do: pi = defaultisdouble
+
+// Arithmetic Operators
+// + - / *
+var area = 15 * 20
+var sum = 5 + 6
+var diff = 10 - 2
+var div = 12 / 4
+var div1 = 12/5
+var div2: Float = 12 / 5
+var remainder = 12 / 5
+
