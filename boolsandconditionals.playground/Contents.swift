d21c69ec17da115bef503a713062894066d1233b
Jason Liang <jjjasonliang@gmail.com>
2018-07-07 12:57:10 +0900

    conditionals

---
+++ boolsandconditionals.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var doIliketakoyaki = true
+
+if true == false || true == true {
+    print("xDD")
+}
+
+var hasDataFinishedDownloading: Bool = false
+
+hasDataFinishedDownloading = true
+
+if 1 == 2 {
+    "should not print"
+}
+
+var foodname = "Takoyaki"
+var wrongname = "takoyaki"
+
+if foodname != wrongname {
+    print("You're saying it wrong!")
+} else if wrongname.characters.count > 5 {
+    print("Find a better name")
+} else {
+    print("Awesome you did it!")
+}
