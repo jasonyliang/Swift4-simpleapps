c015608a99091d67dec7482fd4498005015c3fbe
Jason Liang <jjjasonliang@gmail.com>
2018-07-15 09:57:00 +0900

    color app

---
+++ Color Change Stuff/Color Change Stuff/ColorPickerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ColorPickerViewController.swift
+//  Color Change Stuff
+//
+//  Created by Jason Y Liang on 7/15/18.
+//  Copyright © 2018 Jason Y Liang. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Color Change Stuff/Color Change Stuff/ColorizeApp.swift
@@ -0,0 +1,19 @@
+//
+//  ColorizeApp.swift
+//  Color Change Stuff
+//
+//  Created by Jason Y Liang on 7/14/18.
+//  Copyright © 2018 Jason Y Liang. All rights reserved.
+//
+
+import UIKit
+
+class ColorizeApp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
