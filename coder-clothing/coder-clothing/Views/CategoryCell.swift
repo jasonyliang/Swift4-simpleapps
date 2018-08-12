653e53b80f306e39077a486eb1740c52670c1e3a
Jason Liang <jjjasonliang@gmail.com>
2018-08-13 02:11:40 +0800

    new view

---
+++ coder-clothing/coder-clothing/Views/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coder-clothing
+//
+//  Created by Jason Y Liang on 8/13/18.
+//  Copyright © 2018 Jason Y Liang. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImage!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
