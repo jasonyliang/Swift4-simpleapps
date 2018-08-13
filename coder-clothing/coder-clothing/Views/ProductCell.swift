ab601022fc1130c6c958a7547cf041790e53fd20
Jason Liang <jjjasonliang@gmail.com>
2018-08-13 22:08:46 +0800

    collection view

---
+++ coder-clothing/coder-clothing/Views/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-clothing
+//
+//  Created by Jason Y Liang on 8/13/18.
+//  Copyright © 2018 Jason Y Liang. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
